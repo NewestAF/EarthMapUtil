588e0bbb190554f46c11f0b403151b37d67a2256
SeongJoo Kim <castle5314@naver.com>
2022-10-25 16:56:00 +0900

    feat: Add various utility class

---
+++ src/main/java/com/newestaf/earthmaputil/util/NewestAFException.java
@@ -0,0 +1,11 @@
+package com.newestaf.earthmaputil.util;
+
+public class NewestAFException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public NewestAFException(String message) {
+        super(message);
+    }
+
+}
