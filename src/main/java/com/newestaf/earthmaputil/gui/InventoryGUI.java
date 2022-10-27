419cf6e50cd9b10a7d127ac2e70b489c52de3a22
SeongJoo Kim <castle5314@naver.com>
2022-10-27 17:32:19 +0900

    feat: Add Inventory GUI

---
+++ src/main/java/com/newestaf/earthmaputil/gui/InventoryGUI.java
@@ -0,0 +1,118 @@
+package com.newestaf.earthmaputil.gui;
+
+import com.newestaf.earthmaputil.EarthMapUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+
+public class InventoryGUI implements Listener, InventoryHolder {
+
+    private Player player;
+    private String title;
+    private Inventory inventory;
+    private HashMap<Integer, InventoryGUIButton> buttons;
+    private int slot;
+    private int maxItems;
+    private boolean locked;
+    private BukkitRunnable runnable;
+
+    public InventoryGUI(Player player, String title, int rows, boolean locked) {
+        this.player = player;
+        this.title = title;
+        if (rows < 1) {
+            rows = 1;
+        }
+        if (rows > 6) {
+            rows = 6;
+        }
+        //noinspection deprecation
+        this.inventory = Bukkit.createInventory(null, 9 * rows, title);
+        this.buttons = new HashMap<>();
+        this.slot = 0;
+        this.maxItems = (9 * rows) - 1;
+        this.locked = locked;
+        this.runnable = null;
+//        EarthMapUtil.getInstance().getMenuManager().addMenu(player.getUniqueId(), this);
+    }
+
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public @NotNull Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public HashMap<Integer, InventoryGUIButton> getButtons() {
+        return buttons;
+    }
+
+    public InventoryGUIButton addButton(InventoryGUIButton button) {
+        if (this.slot <= this.maxItems) {
+            this.inventory.setItem(slot, button.getItem());
+            this.buttons.put(slot, button);
+            slot++;
+        }
+        return button;
+    }
+
+    public void addButtons(InventoryGUIButton button, int amount) {
+        for (int i = 0; i < amount; i++) {
+            this.addButton(button);
+        }
+    }
+
+    public InventoryGUIButton setButton(int slot, InventoryGUIButton button) {
+        this.inventory.setItem(slot, button.getItem());
+        this.buttons.put(slot, button);
+        return button;
+    }
+
+    public int getSlot() {
+        return slot;
+    }
+
+    public BukkitRunnable getRunnable() {
+        return runnable;
+    }
+
+    public void setRunnable(BukkitRunnable runnable) {
+        this.runnable = runnable;
+        this.runnable.runTaskTimer(EarthMapUtil.getInstance(), 2, 2);
+    }
+
+    public void stopRunnable() {
+        this.runnable.cancel();
+    }
+
+    public void showMenu() {
+        this.player.openInventory(this.inventory);
+    }
+
+    public void removeAllClickEvents() {
+        for (int i = 0; i < this.slot; i++)
+            this.buttons.get(i).setOnClick(null);
+    }
+
+
+}
